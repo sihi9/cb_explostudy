d0b9ed851a47e012cac8cf7ae0b6221ebf212a9d
Simon Hitzginger <simon.hitzginger@student.tugraz.at>
2022-05-12 16:19:07 +0200

    removed some stuff from git

--- scripts/postArticles.sh
+++
@@ -1,4 +0,0 @@
-curl \
-  -X POST 'http://127.0.0.1:7700/indexes/cb_articles/documents' \
-  -H 'Content-Type: application/json' \
-  --data-binary @data.json
--- src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = global.config = {
-
-  port:3001,
-  meiliSearchPort: 7700,
-  apiKey: 1234
-  
-};
